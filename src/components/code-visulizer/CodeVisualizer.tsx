f2fd79a491b29ec61e82f930beaf64f5fda1d711
bglee <brentglee3@gmail.com>
2025-06-09 21:38:31 -0500

    Move theming def

--- src/components/code-visulizer/CodeVisualizer.tsx
+++ src/components/code-visulizer/CodeVisualizer.tsx
@@ -1,54 +1,21 @@
 import { CodeBlock } from "react-code-blocks";
 import { dracula } from "react-code-blocks";
+import { codeBlockThemeMap, Theme } from "../../config/theming";
 
 type CodeVisualizerProps = {
+  theme: Theme;
   code: string;
   language: string;
 };
 
-const availableThemes = [
-  "a11yDark",
-  "a11yLight",
-  "anOldHope",
-  "androidstudio",
-  "arta",
-  "atomOneDark",
-  "atomOneLight",
-  "codepen",
-  "dracula",
-  "far",
-  "github",
-  "googlecode",
-  "hopscotch",
-  "hybrid",
-  "irBlack",
-  "monoBlue",
-  "monokaiSublime",
-  "monokai",
-  "nord",
-  "noctisViola",
-  "obsidian",
-  "ocean",
-  "paraisoDark",
-  "paraisoLight",
-  "pojoaque",
-  "purebasic",
-  "railscast",
-  "rainbow",
-  "shadesOfPurple",
-  "solarizedDark",
-  "solarizedLight",
-  "sunburst",
-  "tomorrowNightBlue",
-  "tomorrowNightBright",
-  "tomorrowNightEighties",
-  "tomorrowNight",
-  "tomorrow",
-  "vs2015",
-  "xt256",
-  "zenburn",
-];
+export const CodeVisualizer: React.FC<CodeVisualizerProps> = ({ theme, code, language }) => {
+  const codeBlockTheme = codeBlockThemeMap.get(theme);
 
-export const CodeVisualizer: React.FC<CodeVisualizerProps> = ({ code, language }) => {
-  return <CodeBlock text={code} language={language} showLineNumbers={true} theme={dracula} />;
+  if (!codeBlockTheme) {
+    throw new Error(`Theme ${theme} not found`);
+  }
+
+  return (
+    <CodeBlock text={code} language={language} showLineNumbers={true} theme={codeBlockTheme} />
+  );
 };
